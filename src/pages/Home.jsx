0d686c898edb46d853afaf855019b3da76fe1d83
kirushnabalan <kirushnabalan1803@gmail.com>
2025-04-06 14:24:44 +0530

    Implement fixed search bar design with improved UI/UX

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '../commponents/Header'
+import SearchBar from '../commponents/SearchBar'
+
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <SearchBar/>
     </div>
   )
 }
